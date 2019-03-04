a1c72ad41723852cfae3099883482fc5131ec995
Florian Hördt <11618226+Genfood@users.noreply.github.com>
2019-03-04 18:31:09 +0100

    added test app

---
+++ test/Proviant.Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Proviant.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            do
+            {
+                var expr = new BooleanAlgebraExpression(s);
+                //string normalized = expr.NormalizeExpression();
+                //string postfix = expr.ToPostfix();
+                Console.WriteLine($"Ergebniss: {expr.Evaluate()}" );
+                s = Console.ReadLine();
+
+            } while (!String.IsNullOrWhiteSpace(s));
+
+        }
+    }
+}
